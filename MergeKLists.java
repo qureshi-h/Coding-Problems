a5c1b362090fe1f130bc76e775228eb42f255539
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2021-07-16 18:17:00 +1000

    Cleanup

--- MergeKLists.java
+++ MergeKLists.java
@@ -1,3 +1,7 @@
+/**
+ * solution to https://leetcode.com/problems/merge-k-sorted-lists/
+ */
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -12,16 +16,16 @@ import java.util.Queue;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class MergeKLists {
+
+    /*
+    Merges k sorted linked lists into a single sorted linked list
+    80% / 60%
+     */
     public ListNode mergeKLists(ListNode[] lists) {
 
-        Comparator<ListNode> comparator = new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode node1, ListNode node2) {
-                return node1.val-node2.val;
-            }
-        };
-
+        Comparator<ListNode> comparator = (node1, node2) -> node1.val-node2.val;
         Queue<ListNode> queue = new PriorityQueue<ListNode>(comparator);
 
         for (ListNode node: lists)
@@ -32,7 +36,6 @@ class MergeKLists {
 
         ListNode currentNode = head;
         while(!queue.isEmpty()) {
-
             currentNode.next = queue.poll();
             currentNode = currentNode.next;
             if (currentNode.next != null) queue.add(currentNode.next);
