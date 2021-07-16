5c1f4bc1100fa002e3b2e552961a0c7130d87541
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2021-07-16 19:37:21 +1000

    Cleanup

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,7 +1,16 @@
+/**
+ * solution to https://leetcode.com/problems/two-sum/
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
+
+    /*
+    Given an array of integers nums and an integer target,
+    returns the indices of the two numbers such that they add up to target.
+     */
     public int[] twoSum(int[] nums, int target) {
 
         Map<Integer, Integer> hashMap = new HashMap<>();
